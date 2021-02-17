84066c1df93a500a46c3f30dfcf0d71f800ae24f
sonal <sonalkarle01@gmail.com>
2021-02-17 20:48:12 +0530

    Welcome

---
+++ UserregistrationDay10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserregistrationDay10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
